2a15f2966647d9c05d6f613b3c9b55342240d2f3
Julien Dupont <julien.dupont@student-cs.fr>
2025-12-12 10:30:44 +0100

    chore: config dev dependencies

---
+++ commitlint.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'chore']],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+  },
+};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,39 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      'import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc', caseInsensitive: false },
+          'newlines-between': 'always',
+          groups: ['builtin', 'external', 'internal', 'sibling', 'parent', 'index'],
+        },
+      ],
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+        },
+      ],
+    },
+  },
+  eslintPluginPrettierRecommended,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'commitlint.config.mjs',
   ]),
 ]);
 
